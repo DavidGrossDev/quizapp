d23c6c7de0c264343f32955274d529532483b03d
David Groß <d.g1r2o3s4s@gmail.com>
2025-07-13 11:11:22 +0200

    added and changed functions

--- script.js
+++ script.js
@@ -1,26 +1,70 @@
 let currentQuestion = 0;
+let counterRightAnswers = 0;
 
 function init() {
-    document.getElementById('all_questions').innerHTML = questions.length;
-    document.getElementById('current_question').innerHTML = currentQuestion + 1;
     showQuestion();
 }
 
 function showQuestion() {
-    let question = questions[currentQuestion];
 
-    document.getElementById('question_text').innerHTML = question['question'];
-    document.getElementById('answer1').innerHTML = question['answer_1'];
-    document.getElementById('answer2').innerHTML = question['answer_2'];
-    document.getElementById('answer3').innerHTML = question['answer_3'];
-    document.getElementById('answer4').innerHTML = question['answer_4'];
+    if (currentQuestion >= questions.length) {
+        showEndscreen(counterRightAnswers);
+    } else {
+        let question = questions[currentQuestion];
+
+        document.getElementById('question_text').innerHTML = question['question'];
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
+        document.getElementById('all_questions').innerHTML = questions.length;
+        document.getElementById('current_question').innerHTML = currentQuestion + 1;
+    }
 }
 
 function nextQuestion() {
-    if (currentQuestion < questions.length - 1) {
-        currentQuestion++
+    currentQuestion++
+    document.getElementById('next_button').disabled = true;
+    resetAnswerButtons();
+    showQuestion();
+}
+
+function resetAnswerButtons() {
+    document.getElementById('answer_1').parentNode.classList.remove('correct_answer', 'wrong_answer');
+    document.getElementById('answer_2').parentNode.classList.remove('correct_answer', 'wrong_answer');
+    document.getElementById('answer_3').parentNode.classList.remove('correct_answer', 'wrong_answer');
+    document.getElementById('answer_4').parentNode.classList.remove('correct_answer', 'wrong_answer');
+}
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
+    let idRightAnswer = `answer_${question['right_answer']}`;
+
+    if (selectedQuestionNumber == question[`right_answer`]) {
+        document.getElementById(selection).parentNode.classList.add('correct_answer');
+        counterRightAnswers++;
     } else {
-        currentQuestion = 0;
+        document.getElementById(selection).parentNode.classList.add('wrong_answer');
+        document.getElementById(idRightAnswer).parentNode.classList.add('correct_answer');
     }
-    init();
+    document.getElementById('next_button').disabled = false;
+}
+
+function showEndscreen(counterRightAnswers) {
+    document.getElementById('display_content').innerHTML =
+        `<div class="content_main_finish">
+            <div class="content_main_finish_head">
+                <img class="finish_img" src="./assets/img/brainresult.png" alt="">
+                <span class="clr_blck">COMPLETE<br> HTML QUIZ</span>
+            </div>
+            <div class="content_main_finish_score">
+                            <span class="clr_blue">YOUR SCORE</span>
+                            <p class="clr_blck">${counterRightAnswers}/10</p>
+            </div>
+            <div class="content_main_finish_buttons">
+                <button>SHARE</button>
+                <button>REPLAY</button>
+            </div>
+        </div>`
 }
