161fc36c3494ebca81f127ab365ada02c6567b3d
David Groß <d.g1r2o3s4s@gmail.com>
2025-07-14 11:15:58 +0200

    added progressbar functionality

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function showQuestion() {
         document.getElementById('endscreen').style = '';
         document.getElementById('question_body').style = 'display: none';
         document.getElementById('count_result').innerHTML = `${counterRightAnswers}/${questions.length}`;
+        document.getElementById('progress_bar').style = `width: 0%`
     } else {
         let question = questions[currentQuestion];
 
@@ -21,6 +22,7 @@ function showQuestion() {
         document.getElementById('all_questions').innerHTML = questions.length;
         document.getElementById('current_question').innerHTML = currentQuestion + 1;
     }
+    progressBar()
 }
 
 function nextQuestion() {
@@ -52,3 +54,23 @@ function answer(selection) {
     document.getElementById('next_button').disabled = false;
 }
 
+function progressBar() {
+    let percent = (currentQuestion + 1) / questions.length;
+    percent = Math.round(percent * 100);
+    console.log(percent);
+    if (percent < 100) {
+        document.getElementById('progress_bar').style = `width: ${percent}%`;
+    } else if (percent == 100) {
+        document.getElementById('progress_bar').style = `width: ${percent}%`
+        document.getElementById('progress_bar').style.borderRadius = '0';
+    }
+
+}
+
+function restartGame() {
+    currentQuestion = 0;
+    counterRightAnswers = 0;
+    document.getElementById('endscreen').style = 'display: none';
+    document.getElementById('question_body').style = '';
+    init();
+}
