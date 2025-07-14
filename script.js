258f4a5e889df04357db683c865beea31e868bf2
David Groß <d.g1r2o3s4s@gmail.com>
2025-07-14 21:04:28 +0200

    changed for clean coding

--- script.js
+++ script.js
@@ -6,23 +6,35 @@ function init() {
 }
 
 function showQuestion() {
-    if (currentQuestion >= questions.length) {
-        document.getElementById('endscreen').style = '';
-        document.getElementById('question_body').style = 'display: none';
-        document.getElementById('count_result').innerHTML = `${counterRightAnswers}/${questions.length}`;
-        document.getElementById('progress_bar').style = `width: 0%`
+    if (quizOver()) {
+        showEndscreen();
     } else {
-        let question = questions[currentQuestion];
-
-        document.getElementById('question_text').innerHTML = question['question'];
-        document.getElementById('answer_1').innerHTML = question['answer_1'];
-        document.getElementById('answer_2').innerHTML = question['answer_2'];
-        document.getElementById('answer_3').innerHTML = question['answer_3'];
-        document.getElementById('answer_4').innerHTML = question['answer_4'];
-        document.getElementById('all_questions').innerHTML = questions.length;
-        document.getElementById('current_question').innerHTML = currentQuestion + 1;
+        showNextQuestion();
+        progressBar();
     }
-    progressBar()
+}
+
+function quizOver() {
+    return currentQuestion >= questions.length;
+}
+
+function showEndscreen() {
+    document.getElementById('endscreen').style = '';
+    document.getElementById('question_body').style = 'display: none';
+    document.getElementById('count_result').innerHTML = `${counterRightAnswers}/${questions.length}`;
+    document.getElementById('progress_bar').style = `width: 0%`;
+}
+
+function showNextQuestion() {
+    let question = questions[currentQuestion];
+
+    document.getElementById('question_text').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+    document.getElementById('all_questions').innerHTML = questions.length;
+    document.getElementById('current_question').innerHTML = currentQuestion + 1;
 }
 
 function nextQuestion() {
@@ -44,20 +56,36 @@ function answer(selection) {
     let selectedQuestionNumber = selection.slice(-1);
     let idRightAnswer = `answer_${question['right_answer']}`;
 
-    if (selectedQuestionNumber == question[`right_answer`]) {
-        document.getElementById(selection).parentNode.classList.add('correct_answer');
-        counterRightAnswers++;
+    if (correctChoise(selectedQuestionNumber, question)) {
+        rightAnswerSelected(selection);
     } else {
-        document.getElementById(selection).parentNode.classList.add('wrong_answer');
-        document.getElementById(idRightAnswer).parentNode.classList.add('correct_answer');
+        wrongAnswerSelected(selection, idRightAnswer);
     }
+    enableNextButton();
+}
+
+function correctChoise(selectedQuestionNumber, question) {
+    return selectedQuestionNumber == question[`right_answer`];
+}
+
+function rightAnswerSelected(selection) {
+    document.getElementById(selection).parentNode.classList.add('correct_answer');
+    counterRightAnswers++;
+}
+
+function wrongAnswerSelected(selection, idRightAnswer) {
+    document.getElementById(selection).parentNode.classList.add('wrong_answer');
+    document.getElementById(idRightAnswer).parentNode.classList.add('correct_answer');
+}
+
+function enableNextButton() {
     document.getElementById('next_button').disabled = false;
 }
 
 function progressBar() {
     let percent = (currentQuestion + 1) / questions.length;
     percent = Math.round(percent * 100);
-    console.log(percent);
+
     if (percent < 100) {
         document.getElementById('progress_bar').style = `width: ${percent}%`;
     } else if (percent == 100) {
